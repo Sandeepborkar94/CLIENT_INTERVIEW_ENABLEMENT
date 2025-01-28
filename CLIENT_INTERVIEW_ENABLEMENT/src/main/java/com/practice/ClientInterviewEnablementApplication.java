9ec74bc4fb0b4901b61d88b40b4e274c36f69051
Sandeep <Sandyb2711@gmail.com>
2025-01-28 22:35:31 +0530

    initial commit

---
+++ CLIENT_INTERVIEW_ENABLEMENT/src/main/java/com/practice/ClientInterviewEnablementApplication.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientInterviewEnablementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientInterviewEnablementApplication.class, args);
+	}
+
+}
